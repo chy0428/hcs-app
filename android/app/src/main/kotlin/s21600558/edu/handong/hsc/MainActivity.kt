25994dfc5a3181b24c91aab9625c060b3d4a6801
chy0428 <21800758@handong.edu>
2020-10-08 18:25:58 +0900

    Add android/

---
+++ android/app/src/main/kotlin/s21600558/edu/handong/hsc/MainActivity.kt
@@ -0,0 +1,12 @@
+package s21600558.edu.handong.hsc
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
